5f7293fa3e3855b273d9425bbc30c303c0a1fc09
Eduardo Luis Santos Delgado <eduardoluissd@gmail.com>
2020-09-13 14:21:06 -0500

    test ML

--- main.py
+++ main.py
@@ -2,8 +2,53 @@ import time
 
 from predictive.MLManager import MLManager
 from preprocessor.preprocessor import Preprocessor
+from data.DataManager import DataManager
+from RL.environments.sampler import Sampler
 
-p = Preprocessor()
-t = time.perf_counter()
-p.process()
-print("total time: ", time.perf_counter() - t)
+from matplotlib import pyplot as plt
+import numpy as np
+
+# p = Preprocessor()
+# t = time.perf_counter()
+# p.process()
+# print("total time: ", time.perf_counter() - t)
+
+
+data = DataManager.get_joined_data()
+
+s = Sampler(data)
+print(s.reset(random=False))
+s.step(2)
+s.step(2)
+s.step(0)
+s.step(5)
+s.step(5)
+s.step(3)
+s.step(2)
+s.step(2)
+s.step(0)
+s.step(5)
+s.step(5)
+s.step(3)
+s.step(2)
+s.step(2)
+s.step(0)
+s.step(5)
+s.step(5)
+s.step(3)
+s.step(3)
+print(s.get_current_sample_frequency())
+print(s.get_samples_taken())
+
+google_model, X_train, X_test, y_train, y_test = MLManager.logistic_regression(
+    data)
+rl_model, _, _, _, _ = MLManager.logistic_regression(
+    s.get_samples_taken())
+
+reward = MLManager.compate_outpt(
+    google_model.predict(X_test), rl_model.predict(X_test))
+
+print(reward)
+
+print(google_model.predict(np.array([0.031947982202754625]).reshape(1, -1)))
+print(rl_model.predict(np.array([0.031947982202754625]).reshape(1, -1)))
